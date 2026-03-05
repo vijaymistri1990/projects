d5d9d1fc1a9629547749e1548de2f16388b699b0
VijayMakwana <vijay.makwana@tinyscript.in>
2026-03-05 10:57:04 +0530

    Remove white space

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ import { app } from './src/app.js';
 
 app.listen(app.get('port'), function () {
     console.log("Simulator app " + process.env.NODE_ENV + " started on Port No. ", app.get('port'));
-});
+}); 
